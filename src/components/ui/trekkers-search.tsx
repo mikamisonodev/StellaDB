1c8e7ecf30fdb1c7f9d81e1149773a92d52892a7
Misono Mika <herryvu29@gmail.com>
2025-11-29 20:29:44 +0700

    feat: add elemental images and filter configuration for trekkers

--- src/components/ui/trekkers-search.tsx
+++ src/components/ui/trekkers-search.tsx
@@ -3,22 +3,19 @@
 import { Button } from "@heroui/button";
 import { Input } from "@heroui/input";
 import { Select, SelectItem } from "@heroui/select";
+import Image from "next/image";
 import { useState } from "react";
 import { FaSortAmountDown, FaSortAmountUp } from "react-icons/fa";
 import { FaFilterCircleXmark, FaMagnifyingGlass } from "react-icons/fa6";
 
-const sortOptions = [
-    { label: "Default", value: "default" },
-    { label: "Name", value: "name" },
-    { label: "ID", value: "id" },
-];
+import { filterOptions, sortOptions } from "@/config/filters/trekkers";
 
 const TrekkersSearch = () => {
     const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
 
     return (
         <>
-            <div className="flex gap-3 pb-2">
+            <div className="flex gap-3 pb-3">
                 <Input
                     classNames={{ inputWrapper: "bg-default-100/70" }}
                     startContent={<FaMagnifyingGlass />}
@@ -35,10 +32,11 @@ const TrekkersSearch = () => {
                         {sortOrder === "asc" ? <FaSortAmountUp /> : <FaSortAmountDown />}
                     </Button>
                     <Select
+                        aria-label="Sorting"
+                        defaultSelectedKeys={["default"]}
                         classNames={{
                             trigger: "rounded-l-none bg-default-100/70 data-[hover=true]:bg-default-100/50",
                         }}
-                        defaultSelectedKeys={["default"]}
                     >
                         {sortOptions.map(option => (
                             <SelectItem key={option.value}>{option.label}</SelectItem>
@@ -46,8 +44,39 @@ const TrekkersSearch = () => {
                     </Select>
                 </div>
             </div>
-            <div className="flex-1 overflow-auto"></div>
-            <div className="pt-2">
+            <div className="flex-1 space-y-2 h-full">
+                <div className="overflow-auto max-h-[calc(100vh-3.5rem-196px)]">
+                    {filterOptions.map((group, i) => (
+                        <div className="space-y-1.5" key={i}>
+                            <h3 className="text-lg">{group.label}</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {group.items.map(item => (
+                                    <Button
+                                        className="bg-default-100/50"
+                                        key={item.value}
+                                        startContent={
+                                            item.img ? (
+                                                <Image
+                                                    src={item.img}
+                                                    alt={item.label}
+                                                    width={24}
+                                                    height={24}
+                                                    priority
+                                                />
+                                            ) : item.icon ? (
+                                                <item.icon size={18} />
+                                            ) : null
+                                        }
+                                    >
+                                        {item.label}
+                                    </Button>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="pt-3">
                 <p className="text-center text-sm mb-2">0 / 0 trekkers displayed</p>
                 <Button className="bg-default-100/70 w-full" startContent={<FaFilterCircleXmark />}>
                     Clear 0 filter(s)
---
+++ src/config/filters/trekkers.ts
@@ -0,0 +1,42 @@
+"use client";
+
+import { IconType } from "react-icons";
+import { LuSword } from "react-icons/lu";
+import { TbBowFilled } from "react-icons/tb";
+
+export const sortOptions = [
+    { label: "Default", value: "default" },
+    { label: "Name", value: "name" },
+    { label: "ID", value: "id" },
+];
+
+type FilterOptions = {
+    label: string;
+    items: {
+        label: string;
+        value: string;
+        icon?: IconType;
+        img?: string;
+    }[];
+};
+
+export const filterOptions: FilterOptions[] = [
+    {
+        label: "Attack Range",
+        items: [
+            { label: "Melee", value: "melee", icon: LuSword },
+            { label: "Ranged", value: "ranged", icon: TbBowFilled },
+        ],
+    },
+    {
+        label: "Element",
+        items: [
+            { label: "Aqua", value: "aqua", img: "/elements/Aqua.png" },
+            { label: "Ignis", value: "ignis", img: "/elements/Ignis.png" },
+            { label: "Terra", value: "terra", img: "/elements/Terra.png" },
+            { label: "Ventus", value: "ventus", img: "/elements/Ventus.png" },
+            { label: "Umbra", value: "nox", img: "/elements/Umbra.png" },
+            { label: "Lux", value: "lux", img: "/elements/Lux.png" },
+        ],
+    },
+];
